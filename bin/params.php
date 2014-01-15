fe642a44cd2f505d4269f7d76ffbd8e807e08fc3
pat42410 <pat42410@hotmail.fr>
2014-01-15 15:02:00 +0100

    MaJ BDD + Paramètre de connexion à la BDD

--- bin/params.php
+++ bin/params.php
@@ -2,5 +2,8 @@
 $host='localhost';
 $user='';
 $password='';
-$base='';
+$base='site_rando';
+
+$server = mysql_connect($hote,$user,$mdp) or die("Erreur de connexion au serveur $Hôte");
+$database = mysql_select_db($bdd, $server) or die("Erreur de connexion à la base de donnée $BDD");
 ?>
