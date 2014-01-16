851488bc19bc4d231f9a74d51aa2305c192f38e0
dualshote <benron@hotmail.fr>
2014-01-16 21:09:44 +0100

    modif encodage

--- Controller/c_recherche_avancee.php
+++ Controller/c_recherche_avancee.php
@@ -3,6 +3,6 @@
 include_once('bin/params.php');
 include_once('Models/m_select_randos.php');
 
-$listeRando = select_randos('code, descriptif');
+$listeRando = select_randos('code, difficulté');
 
 include_once('View/v_recherche_avancee.php');
