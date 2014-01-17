b1ea42aa49bf68ac30ab1b0c63761eb317203a48
pat42410 <pat42410@hotmail.fr>
2014-01-17 14:07:33 +0100

    MaJ menu

--- View/menu.php
+++ View/menu.php
@@ -3,9 +3,9 @@ function menu(){
 	echo '	<nav>
 				<ul>
 					<a href="index.php"><li>Acceuil</li></a>
-					<a href="randonnee.php"><li>Randonnée</li></a>
+					<a href="randonnee.php"><li>Randonnées</li></a>
 					<a href="galerie.php"><li>Galerie</li></a>
-					<a href="autres_activités.php"><li>Autres activités</li></a>
+					<a href="ajout_rando.php"><li>Ajouter une rando</li></a>
 				</ul>
 			</nav>
 	';
