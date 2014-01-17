8ea7f7d708114fb52d2684b9723e66baedc0aaf7
dualshote <benron@hotmail.fr>
2014-01-17 15:21:09 +0100

    modif fonction recherche

--- Models/m_gestion_recherche.php
+++ Models/m_gestion_recherche.php
@@ -4,7 +4,7 @@
 		if(isset($_POST['title']))
 		{
 			$requete = $bdd->prepare("SELECT titre FROM rando WHERE titre= :title");
-			$requete = execute(array('title' => $_POST['title'])) or die(print_r($erreur -> errorInfo()));
+			$requete->execute(array('title' => $_POST['title'])) or die(print_r($erreur -> errorInfo()));
 			$res = $requete->fetchAll();
 			$requete->closeCursor();
 			return $res;
