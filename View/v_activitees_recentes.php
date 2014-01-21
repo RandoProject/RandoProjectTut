8c0b85cdac77416be22bbe129596dabd9a796832
pat42410 <pat42410@hotmail.fr>
2014-01-21 14:10:24 +0100

    MaJ activtees recentes

--- View/v_activitees_recentes.php
+++ View/v_activitees_recentes.php
@@ -3,7 +3,7 @@
     <?php
     foreach($liste_rando as $rando) {
 		echo $rando['titre'] .'</br>';
-		echo '<img src="Resources/Galerie/'. $rando['nom_galerie'] .'/'. $rando['nom_photo'] .'" width="100px" height="50px;"/></br></br>';
+		echo '<img src="Resources/Galerie/'. $rando['nom_galerie'] .'/'. $rando['nom_photo'] .'" width="144px" height="81px;"/></br></br>';
 	}
 	?>
 </aside>			
