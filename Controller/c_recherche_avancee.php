e181d71490d2dcaf06e9f47c85bd0562ee5571cd
pat42410 <pat42410@hotmail.fr>
2014-01-21 23:04:53 +0100

    MaJ affichage photo fiche rando

--- Controller/c_recherche_avancee.php
+++ Controller/c_recherche_avancee.php
@@ -125,10 +125,4 @@ $affichage_rando_complet = affichage_f_rando_complet($_POST['s_region'], $typeRe
 $listeRegion = select_regions('num_region, nom');
 
 
-/*Affichage des photos*/
-if(isset($_POST['title'])){
-	$photo_rando = affichage_photo($_POST['title']);
-}
-
-
 include_once('View/v_recherche_avancee.php');
