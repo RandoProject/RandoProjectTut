31f53eb83b21452a90be3c4abc1e2b41bd582d73
pat42410 <pat42410@hotmail.fr>
2014-01-23 15:39:32 +0100

    MaJ CSS

--- View/v_activitees_recentes.php
+++ View/v_activitees_recentes.php
@@ -1,9 +1,11 @@
 <aside>
-    Dernières Randonnées </br>
-    <?php
-    foreach($liste_rando as $rando) {
-		echo $rando['titre'] .'</br>';
-		echo '<img src="Resources/Galerie/'. $rando['nom_galerie'] .'/'. $rando['nom_photo'] .'" width="144px" height="81px;"/></br></br>';
-	}
-	?>
+    <p>Dernières Randonnées</p>
+    <center>
+		<?php
+        foreach($liste_rando as $rando) {
+            echo $rando['titre'].'</br>';
+            echo '<img src="Resources/Galerie/'.$rando['nom_galerie'].'/'.$rando['nom_photo'].'" width="144px" height="81px;"/><br/>';
+        }
+        ?>
+    </center>
 </aside>			
