fb8f1f388d914fbd6e0b79a92f8bb392896652f0
pat42410 <pat42410@hotmail.fr>
2014-01-23 16:06:39 +0100

    Modification BDD + footer + CSS

--- View/footer.php
+++ View/footer.php
@@ -1,7 +1,10 @@
 <?php
-
+// nb membre/rando/photos ; a propos ; nb gens en ligne
 function footer(){
 	echo '	<footer>
+				<p>
+					<a href="#">Qui sommes-nous ?</a> | <a href="#">Contact</a>
+				</p>
 				<p>
 					&copy; 2014 RandoPassion
 				</p>
