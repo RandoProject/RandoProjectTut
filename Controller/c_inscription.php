93ed71e8d66ce305b7bb9dbc843c5a216094bbed
tlgman <tlgman@hotmail.fr>
2014-01-30 13:46:10 +0100

    dd

---
+++ Controller/c_inscription.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once('bin/params.php');
+include_once('Models/m_inscription.php');
+
+
+
+include_once('v_inscription.php');
---
+++ View/v_inscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <?php head("Recherche avancée"); ?>
+
+    <body>
+        <?php menu(); ?>
+        
+        <div id="corps">
+            <section >
+                    <h2>Bienvenue sur RandoPassion, inscrivez-vous dès maintenant !</h2> 
+
+                    <form action="" method="post">
+
+
+
+                    </form>  
+            </section>
+        </div>
+    
+        <?php include_once('Controller/c_footer.php'); ?>
+    </body>
+</html>
