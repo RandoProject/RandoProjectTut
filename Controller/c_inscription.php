a0cbefcb80437d695a93262d8bd950cfea6bf0c2
dualshote <benron@hotmail.fr>
2014-01-30 20:00:54 +0100

    modif inscription

--- Controller/c_inscription.php
+++ Controller/c_inscription.php
@@ -3,6 +3,19 @@
 include_once('bin/params.php');
 include_once('Models/m_inscription.php');
 
+	if(!isset($_SESSION['statut'])){
+		if(strtolower($_SERVER['REQUEST_METHOD']) == 'post'){
+			if(isset($_POST['pseudo']) and $_POST['pseudo'] != ""){
+				$pseudo = strip_tags($_POST['name']);
+				if(strlen($pseudo) > 2)
+			}
+		}
+		else{
+			include_once('View/v_inscription.php');
+		}
+	}
+	else{
+		echo 'Vous ne pouvez pas vous inscrire car vous êtes déjà connecté !';
+	}
 
-
-include_once('v_inscription.php');
+include_once('View/v_inscription.php');
--- View/v_inscription.php
+++ View/v_inscription.php
@@ -8,12 +8,39 @@
         
         <div id="corps">
             <section >
-                    <h2>Bienvenue sur RandoPassion, inscrivez-vous dès maintenant !</h2> 
+                    <h2>Bienvenue sur RandoPassion, inscrivez-vous dès maintenant !</h2><br/> 
 
-                    <form action="" method="post">
+                    <form action="index.php?page=inscription" method="post">
+                        <label for="pseudo">Pseudo : </label>
+                            <input type="texte" id="pseudo" name="pseudo"/><br/><br/>
 
+                        <label for="password">Mot de passe :</label>
+                            <input type="password" id="password" name="password"/><br/><br/>
 
+                        <label for="familly_name">Nom : </label>
+                            <input type="texte" id="familly_name" name="familly_name"/><br/><br/>
 
+                        <label for="name">Prénom : </label>
+                            <input type="texte" id="name" name="name"/><br/><br/>
+
+                        <label for="day_birth">Date de naissance :</label>
+                            <input type="texte" id="day_birth" name="day_birth"/>
+                            <input type="texte" name="month_birth"/>
+                            <input type="texte" name="year_birth"/><br/><br/>
+
+                        <label for="street">Adresse :</label>
+                            <input type="texte" id="street" name="street"/><br/><br/>
+
+                        <label for="postal_code">Code postal :</label>
+                            <input type="texte" id="postal_code" name="postal_code"/><br/><br/>
+
+                        <label for="city">Ville :</label>
+                            <input type="texte" id="city" name="city"/><br/><br/>
+
+                        <label for="mail">Mail :</label>
+                            <input type="texte" id="mail" name="mail"/><br/><br/>
+
+                        <input type="submit" value"S'inscrire"/>
                     </form>  
             </section>
         </div>
