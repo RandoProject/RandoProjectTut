fd17f400a0e6d14b7a9193ff9f95c537555a5302
pat42410 <pat42410@hotmail.fr>
2014-01-30 22:06:14 +0100

    dd

--- Models/m_randonnees.php
+++ Models/m_randonnees.php
@@ -4,10 +4,10 @@ function get_rando($code){
 	global $bdd;
 
 	$queryStr = '	SELECT rando.*, photo.nom AS nom_photo , galerie.nom AS nom_galerie
-				FROM rando, photo, galerie
-				WHERE rando.photo_principale = photo.numero
-				AND photo.galerie = galerie.numero
-				AND rando.code = :code';
+					FROM rando, photo, galerie
+					WHERE rando.photo_principale = photo.numero
+					AND photo.galerie = galerie.numero
+					AND rando.code = :code';
 	$queryArray = array('code' => $code);
 	
 	$query = $bdd->prepare($queryStr);
