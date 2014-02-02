b298a81d51b569ce9895451ce3651c1ad1070cee
tlgman <tlgman@hotmail.fr>
2014-02-02 16:21:28 +0100

    Correction bug certains GPX

--- Controller/c_ajouter_rando.php
+++ Controller/c_ajouter_rando.php
@@ -3,17 +3,25 @@
 if(isset($_SESSION['statut']) and in_array($_SESSION['statut'], array('administrateur', 'moderateur', 'utilisateur'))){
 	
 	if(strtolower($_SERVER['REQUEST_METHOD']) == 'post'){
+		$error = array();
 		if(isset($_POST['name']) and $_POST['name'] != ""){
 			$name = strip_tags($_POST['name']);
-			if(strlen($name) > 255){
-				$error['name'] = "Le nom de la randonnée doit faire moins de 255 caractères.";
+			if(strlen($name) > 150){
+				$error['name'] = "Le nom de la randonnée doit faire moins de 150 caractères.";
+			}
+			else{
+				include_once('bin/params.php');
+				include_once('Models/m_randonnees.php');
+				if(count_rando(strtolower($name))->nb_rando != 0){
+					$error['name'] = "Le nom de randonnée que vous voulez enregistrer existe déjà.";
+				}
 			}
 		}
 		else{
 			$error['name'] = "Vous n'avez pas donné de nom à la randonnée";
 		}
 
-		if(!isset($_POST['descritption']) or $_POST['description'] == ""){
+		if(!isset($_POST['description']) or $_POST['description'] == ""){
 			$error['description'] = "Vous devez entrer une description.";
 		}
 
@@ -56,12 +64,12 @@ if(isset($_SESSION['statut']) and in_array($_SESSION['statut'], array('administr
 			$error['hour'] = "L'heure entrée est invalide";
 		}
 
-		if(!isset($_POST['hour']) or $_POST['hour'] == ""){
-			$hour = 0;
+		if(!isset($_POST['minutes']) or $_POST['minutes'] == ""){
+			$minutes = 0;
 		}
 		else if(is_numeric($_POST['minutes'])){
-			$hour = intval($_POST['minutes']);
-			if($hour < 0 or $hour > 23){
+			$minutes = intval($_POST['minutes']);
+			if($minutes < 0 or $minutes > 59){
 				$error['minutes'] = "Le nombre de minutes doit être compris entre 0 et 59.";
 			}
 		}
@@ -73,14 +81,37 @@ if(isset($_SESSION['statut']) and in_array($_SESSION['statut'], array('administr
 			$water = ($_POST['water'] == 'oui')? 1 : 0;
 		}
 		else{
-			$error['water'] = "Vous n'avez pas précisé s'il y avait un point d'eau.";
+			$error['water'] = "Vous n'avez pas précisé si votre randonnée comportait un point d'eau.";
 		}
 
-		
+		if(empty($error)){
+				/// Si bon...
+		}
+		else{
+			if(!isset($error['name'])){
+				$value['name'] = $name;
+			}
+			if(!isset($error['description'])){
+				$value['description'] = $_POST['description'];
+			}
+			if(!isset($error['difficulty'])){
+				$value['difficulty'] = $difficulty;
+			}
+			if(!isset($error['day'])){
+				$value['day'] = $day;
+			}
+			if(!isset($error['hour'])){
+				$value['hour'] = $hour;
+			}
+			if(!isset($error['minutes'])){
+				$value['minutes'] = $minutes;
+			}
+			if(!isset($error['water'])){
+				$value['water'] = $water;
+			}
 
-
-
-
+			include_once('View/v_ajouter_rando.php');
+		}
 	}
 	else{
 		include_once('View/v_ajouter_rando.php');
