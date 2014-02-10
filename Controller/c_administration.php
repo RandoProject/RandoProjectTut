d29a4574c76cfe63d97fb3a03af5f9cd66fabbe4
pat42410 <pat42410@hotmail.fr>
2014-02-10 12:05:00 +0100

    Début page administration

--- Controller/c_administration.php
+++ Controller/c_administration.php
@@ -1,86 +1,15 @@
 <?php
 
-if(!$_SESSION){
+if($_SESSION['statut'] !== 'administrateur'){
 	header('location:index.php?page=connexion');
 }
 
 include_once('bin/params.php');
-include_once('Models/m_members.php');
+include_once('Models/m_administration.php');
 
-$member = get_member($_SESSION['pseudo']);
-$photo = get_photo($_SESSION['pseudo']);
+$listeRando = get_liste_rando();
+$listeMember = get_liste_member();
+$listeComment = get_liste_comment();
+$galery = get_galery();
 
-
-$pseudo = $member->pseudo;
-$name = $member->nom;
-$firstname = $member->prenom;
-
-// Photo
-$path_photo = 'Resources/Galerie/'.$photo->galerie.'/'.$photo->photo;
-
-// Date de naissance
-if(empty($member->date_naiss)){
-	$birth = '<em>non renseignée</em>';
-}
-else{
-	$date = new DateTime(trim($member->date_naiss));
-	$day = $date->format('d');
-	$month = $date->format('m');
-	$year = $date->format('Y');
-	switch($month) {
-		case '01': $month = 'Janvier'; break;
-		case '02': $month = 'Février'; break;
-		case '03': $month = 'Mars'; break;
-		case '04': $month = 'Avril'; break;
-		case '05': $month = 'Mai'; break;
-		case '06': $month = 'Juin'; break;
-		case '07': $month = 'Juillet'; break;
-		case '08': $month = 'Août'; break;
-		case '09': $month = 'Septembre'; break;
-		case '10': $month = 'Octobre'; break;
-		case '11': $month = 'Novembre'; break;
-		case '12': $month = 'Décembre'; break;
-		default: $month =''; break;
-	}
-	$birth = $day.' '.$month.' '.$year;
-}
-
-// Adresse
-if(empty($member->adresse) && empty($member->code_postal) && empty($member->ville)){
-	$adress = '<em>non renseignée</em>';
-}
-else{
-	if(!empty($member->adresse)){
-		$adress = $member->adresse;
-	}
-	if(!empty($member->code_postal)){
-		if(!empty($member->adresse))
-			$adress .= ', '.$member->code_postal;
-		else
-			$adress = $member->code_postal;
-	}
-	if(!empty($member->ville)){
-		if(!empty($member->adresse) || !empty($member->code_postal))
-			$adress .= ', '.$member->ville;
-		else
-			$adress = $member->ville;
-	}
-}
-
-// E-mail
-if(empty($member->mail)){
-	$mail = '<em>non renseigné</em>';
-}
-else{
-	$mail = '<a href="mailto:'.$member->mail.'">'.$member->mail.'</a>';
-}
-
-// Déscription
-if(empty($member->description)){
-	$description = '<em>non renseignée</em>';
-}
-else{
-	$description = $member->description;
-}
-
-include_once('View/v_profil.php');
+include_once('View/v_administration.php');
