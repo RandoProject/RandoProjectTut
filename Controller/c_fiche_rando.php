b6cbd6ac39d8d0264f711658eccad91a377f7fae
dualshote <benron@hotmail.fr>
2014-02-27 09:39:04 +0100

    modif note

--- Controller/c_fiche_rando.php
+++ Controller/c_fiche_rando.php
@@ -85,18 +85,28 @@ if(isset($_GET['code'])){
 		if(strtolower($_SERVER['REQUEST_METHOD']) == 'post'){
 			if(!empty($_POST['commentaire'])){
 				$commentaire = strip_tags($_POST['commentaire']);
-				if($commentaire != ""){
-					validation_commentaire($commentaire,$_SESSION['pseudo'], $code);
+			}
+			if(!isset($_POST['note'])){
+				$note = 0;
+			}
+			else if(is_numeric($_POST['note']) and intval($_POST['note']) <= 5 and intval($_POST['note']) >= 1){
+				$note = intval($_POST['note']);
+			}
+
+			if($commentaire != ""){
+					validation_commentaire($commentaire,$_SESSION['pseudo'], $code, $note);
 					$nombre_commentaire = recuperation_commentaire($code);
 					$insertion_date = $date->format('d').' '.$month.' '.$date->format('Y');
+					$somme = intval(some_note_rando($code));
+					$nombre_note = intval(calcul_note_rando($code));
+					$note_final = $somme/$nombre_note;
+					mise_a_jour_note($code, $note_final);
 					include_once('View/v_fiche_rando.php');
-				}
 			}
 		}
 	}
 
 	$nombre_commentaire = recuperation_commentaire($code);
-	$insertion_date = $date->format('d').' '.$month.' '.$date->format('Y');
 }
 
 include_once('View/v_fiche_rando.php');
--- Models/m_commentaire.php
+++ Models/m_commentaire.php
@@ -1,9 +1,9 @@
 <?php
-	function validation_commentaire($commentaire, $pseudo, $code){
+	function validation_commentaire($commentaire, $pseudo, $code, $note){
 		global $bdd;
 
-		$reqStr = "INSERT INTO commentaire(`date`, auteur, code_rando, commentaire) VALUES(NOW(), :pseudo, :code, :commentaire)";
-		$reqArray = array('pseudo' => $pseudo, 'code' => $code, 'commentaire' => $commentaire);
+		$reqStr = "INSERT INTO commentaire(`date`, auteur, code_rando, commentaire, note) VALUES(NOW(), :pseudo, :code, :commentaire, :note)";
+		$reqArray = array('pseudo' => $pseudo, 'code' => $code, 'commentaire' => $commentaire, 'note' => $note);
 
 		$req = $bdd->prepare($reqStr);
 		$req->execute($reqArray)  or die(print_r($erreur -> errorInfo()));
@@ -22,4 +22,42 @@
 		$res = $req->fetchAll();
 		$req->closeCursor();
 		return $res;
+	}
+
+
+	function some_note_rando($code){
+		global $bdd;
+
+		$reqStr = "SELECT SUM(note) FROM commentaire WHERE code_rando = :code";
+		$reqArray = array('code' => $code);
+
+		$req = $bdd->prepare($reqStr);
+		$req->execute($reqArray) or die(print_r($erreur -> errorInfo()));
+		$res = $req->fetchAll();
+		$req->closeCursor();
+		return $res;
+	}
+
+	function calcul_note_rando($code){
+		global $bdd;
+
+		$reqStr = "SELECT COUNT(note) FROM commentaire WHERE code_rando = :code";
+		$reqArray = array('code' => $code);
+
+		$req = $bdd->prepare($reqStr);
+		$req->execute($reqArray) or die(print_r($erreur -> errorInfo()));
+		$res = $req->fetchAll();
+		$req->closeCursor();
+		return $res;
+	}
+
+	function mise_a_jour_note($code, $note_final){
+		global $bdd;
+
+		$reqStr = "UPDATE rando SET note = :note_final WHERE code = :code";
+		$reqArray = array('note_final' => $note_final, 'code' => $code);
+
+		$req = $bdd->prepare($reqStr);
+		$req->execute($reqArray)  or die(print_r($erreur -> errorInfo()));
+		$req->closeCursor();
 	}
