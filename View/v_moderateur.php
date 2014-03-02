777a685ee7cd293ebf7e1aefbcfa44651b8aecff
pat42410 <pat42410@hotmail.fr>
2014-03-02 15:24:30 +0100

    maj modo

--- View/v_moderateur.php
+++ View/v_moderateur.php
@@ -2,11 +2,38 @@
 <html lang="fr">
 
 	<?php head("Modération"); ?>
-
+    
+	<script language="javascript">
+        function unCheck(id){
+            if(!document.getElementById(id).checked){
+                document.getElementById(id).checked = true;
+            }
+            else{
+                document.getElementById(id).checked = false;
+            }
+        }
+    
+	function unCheckAll(){ 
+		var inputs = document.getElementsByTagName("input");
+		var value = false;
+		for(var i = 0; i < inputs.length; i++){
+			if(inputs[i].type == "checkbox"){
+				if(inputs[i].checked == false){
+					value = true;
+				}
+				break;
+			}
+		}
+		for(var i = 0; i < inputs.length; i++){
+			if(inputs[i].type == "checkbox"){
+				inputs[i].checked = value;
+			}
+		}
+	}
+    </script>
 	<body>
         <div id="corps">   
 			<?php menu(); ?>
-         
             <section id="administration">
             	<a href="index.php?page=moderateur&section=commentaires">[commentaire]</a>
             	<a href="index.php?page=moderateur&section=randonnees">[randonnees]</a>
@@ -20,22 +47,16 @@
                         <input type="submit" value="Valider les randonnées" name="validate"/>
                         <input type="submit" value="Modifier les randonnées" name="update"/>
                         <input type="submit" value="Supprimer les randonnées" name="delete"/>
-                        <input type="reset" value="Déselectionner tout"/>
+                        <input type="button" value="Déselectionner tout" onclick="check('a');"/>
+                        
                         <table>
-                            <tr align="center">
-                                <td></td>
-                                <td>Titre</td>
-                                <td >Auteur</td>
-                                <td>Difficulté</td>
-                                <td>Longueur</td>
-                                <td>Durée</td>
-                                <td>Dénivelé</td>
-                                <td>Département</td>
-                                <td>Point d'eau</td>
-                                <td>Equipement</td>
+                            <tr>
+                                <td width="25px"></td>
+                                <td width="200px">Titre</td>
+                                <td width="250px">Equipement</td>
                                 <td>Déscription</td>
-                                <td>Date insertion</td>
-                                <td>Photo</td>
+                                <td width="110px">Photo</td>
+                                <td width="20px"><input type="checkbox" onClick="unCheckAll();"/></td>
                             </tr>	
                             <?php
 								if(!empty($_POST['update'])){
@@ -54,7 +75,6 @@
 													<td>'.$rando['point_eau'].'</td>
 													<td>'.$rando['equipement'].'</td>
 													<td><textarea name="description">'.$rando['descriptif'].'</textarea></td>
-													<td>'.$rando['date_insertion'].'</td>
 													<td><img src="Resources/Galerie/'.$rando['nom_galerie'].'/'.$rando['nom_photo'].'" width="100px"/></td>
 													<td><input type="checkbox" value="'.$rando['code'].'" name="rando[]"/></td>
 												</tr>';
@@ -65,21 +85,13 @@
 									foreach($listeRando as $rando){
 										if(($i % 2) === 0) $ligneColor = 'pair';
 										else $ligneColor = 'impair';
-										echo '	<tr class="ligne_'.$ligneColor.'">
+										echo '	<tr class="ligne_'.$ligneColor.'" height="80px" onclick="unCheck(\''.$i.'\');">
 													<td>'.$rando['code'].'</td>
 													<td>'.$rando['titre'].'</td>
-													<td>'.$rando['auteur'].'</td>
-													<td>'.$rando['difficulte'].'</td>
-													<td>'.$rando['longueur'].'</td>
-													<td>'.$rando['duree'].'</td>
-													<td>'.$rando['denivele'].'</td>
-													<td>'.$rando['departement'].'</td>
-													<td>'.$rando['point_eau'].'</td>
 													<td>'.$rando['equipement'].'</td>
-													<td>'.truncate($rando['descriptif'], 50).'</td>
-													<td>'.print_date($rando['date_insertion']).'</td>
+													<td>'.truncate($rando['descriptif'], 400).'</td>
 													<td><img src="Resources/Galerie/'.$rando['nom_galerie'].'/'.$rando['nom_photo'].'" width="100px"/></td>
-													<td><input type="checkbox" value="'.$rando['code'].'" name="rando[]"/></td>
+													<td><input type="checkbox" value="'.$rando['code'].'" name="rando[]" class="a" id="'.$i.'" onclick="check(\''.$i.'\');"/></td>
 												</tr>';
 										$i++;
 									}
@@ -96,9 +108,10 @@
                         <input type="reset" value="Déselectionner tout"/>
                         <table>
                             <tr>
-                                <td>Numéro</td>
-                                <td>Auteur</td>
+                                <td width="25px"></td>
+                                <td width="170px">Auteur</td>
                                 <td>Commentaire</td>
+                                <td width="20px"></td>
                             </tr>
                             <?php
                                 foreach($listeComment as $comment){
@@ -107,7 +120,7 @@
                                     echo '	<tr class="ligne_'.$ligneColor.'" title="'.$comment['commentaire'].'">
                                                 <td>'.$comment['numero'].'</td>
                                                 <td>'.$comment['auteur'].'</td>
-                                                <td>'.truncate($comment['commentaire'], 200).'</td>
+                                                <td>'.truncate($comment['commentaire'], 250).'</td>
                                                 <td><input type="checkbox" value="'.$comment['numero'].'" name="comment[]"/></td>
                                             </tr>';
 									$i++;
@@ -115,6 +128,10 @@
                             ?>
                         </table>
                     </form>
+                    
+                 <!-- Partie PHOTOS -->
+            	<?php } else if($_GET['section'] === 'photos'){ ?>
+                	azertyuiop
                 <?php } ?>
             </section>
 
