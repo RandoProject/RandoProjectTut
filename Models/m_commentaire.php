789be02f3fa8077b879d10f4c077abb2109b266c
pat42410 <pat42410@hotmail.fr>
2014-03-02 12:47:15 +0100

    Correction ajout commentaire

--- Models/m_commentaire.php
+++ Models/m_commentaire.php
@@ -1,50 +1,51 @@
 <?php
-	function validation_commentaire($commentaire, $pseudo, $code, $note){
-		global $bdd;
 
-		$reqStr = "INSERT INTO commentaire(`date`, auteur, code_rando, commentaire, note) VALUES(NOW(), :pseudo, :code, :commentaire, :note)";
-		$reqArray = array('pseudo' => $pseudo, 'code' => $code, 'commentaire' => $commentaire, 'note' => $note);
+function validation_commentaire($commentaire, $pseudo, $code, $note){
+	global $bdd;
 
-		$req = $bdd->prepare($reqStr);
-		$req->execute($reqArray)  or die(print_r($erreur->errorInfo()));
-		$req->closeCursor();
-	}
+	$reqStr = "INSERT INTO commentaire(`date`, auteur, code_rando, commentaire, note, valide) VALUES(NOW(), :pseudo, :code, :commentaire, :note, 0)";
+	$reqArray = array('pseudo' => $pseudo, 'code' => $code, 'commentaire' => $commentaire, 'note' => $note);
 
-
-	function recuperation_commentaire($code){
-		global $bdd;
-
-		$reqStr = "SELECT * FROM commentaire WHERE code_rando = :code ORDER BY `date` DESC";
-		$reqArray = array('code' => $code);
-
-		$req = $bdd->prepare($reqStr);
-		$req->execute($reqArray)  or die(print_r($erreur->errorInfo()));
-		$res = $req->fetchAll();
-		$req->closeCursor();
-		return $res;
-	}
+	$req = $bdd->prepare($reqStr);
+	$req->execute($reqArray)  or die(print_r($erreur->errorInfo()));
+	$req->closeCursor();
+}
 
 
-	function moyenne_note_rando($code){
-		global $bdd;
+function recuperation_commentaire($code){
+	global $bdd;
 
-		$reqStr = "SELECT AVG(note) as moyenne_note FROM commentaire WHERE code_rando = :code AND note != 0";
-		$reqArray = array('code' => $code);
+	$reqStr = "SELECT * FROM commentaire WHERE code_rando = :code ORDER BY `date` DESC";
+	$reqArray = array('code' => $code);
 
-		$req = $bdd->prepare($reqStr);
-		$req->execute($reqArray) or die(print_r($erreur->errorInfo()));
-		$res = $req->fetch();
-		$req->closeCursor();
-		return $res;
-	}
+	$req = $bdd->prepare($reqStr);
+	$req->execute($reqArray)  or die(print_r($erreur->errorInfo()));
+	$res = $req->fetchAll();
+	$req->closeCursor();
+	return $res;
+}
 
-	function mise_a_jour_note($code, $moyenne){
-		global $bdd;
 
-		$reqStr = "UPDATE rando SET note = :moyenne WHERE code = :code";
-		$reqArray = array('moyenne' => $moyenne, 'code' => $code);
+function moyenne_note_rando($code){
+	global $bdd;
 
-		$req = $bdd->prepare($reqStr);
-		$req->execute($reqArray)  or die(print_r($erreur->errorInfo()));
-		$req->closeCursor();
-	}
+	$reqStr = "SELECT AVG(note) as moyenne_note FROM commentaire WHERE code_rando = :code AND note != 0";
+	$reqArray = array('code' => $code);
+
+	$req = $bdd->prepare($reqStr);
+	$req->execute($reqArray) or die(print_r($erreur->errorInfo()));
+	$res = $req->fetch();
+	$req->closeCursor();
+	return $res;
+}
+
+function mise_a_jour_note($code, $moyenne){
+	global $bdd;
+
+	$reqStr = "UPDATE rando SET note = :moyenne WHERE code = :code";
+	$reqArray = array('moyenne' => $moyenne, 'code' => $code);
+
+	$req = $bdd->prepare($reqStr);
+	$req->execute($reqArray)  or die(print_r($erreur->errorInfo()));
+	$req->closeCursor();
+}
