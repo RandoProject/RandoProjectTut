d4f9b4743ff6c657721a6d7984f75aa454e53a81
dualshote <benron@hotmail.fr>
2014-03-02 09:32:55 +0100

    modif contact

--- View/v_contact.php
+++ View/v_contact.php
@@ -10,14 +10,15 @@
             <?php include_once('Controller/c_activitees_recentes.php'); ?>
 
             <section id="contact">
-                <form action="index.php?page=contact" method="post">
-                    <input type="text" name="nom" placeholder="Nom" autocomplete="off" maxlength="30" required/><br/>
-                    <input type="email" name="adresse_mail" placeholder="Email" autocomplete="off" required/><br/>
-                    <input type="text" name="objet" placeholder="Objet" autocomplete="off" required/><br/>
-                    <textarea name="message" id="textarea_mail"  style="width: 700px; height: 200px" placeholder="Message" autocomplete="off" required></textarea><br/>
-                    <?php if(isset($mail_verif)) echo '<p> Votre adresse email n\'est pas valide </p>';?>
-                    <input type="submit" name="envoi_mail" value="Envoyer"/>      
-                </form>
+                <div class="titre">Nous contacter</div><br/><br/>
+                    <form action="index.php?page=contact" method="post">
+                        <input type="text" name="nom" placeholder="Nom" autocomplete="off" maxlength="30" required/><br/>
+                        <input type="email" name="adresse_mail" placeholder="Email" autocomplete="off" required/><br/>
+                        <input type="text" name="objet" placeholder="Objet" autocomplete="off" required/><br/>
+                        <textarea name="message" id="textarea_mail"  style="width: 700px; height: 200px" placeholder="Message" autocomplete="off" required></textarea><br/>
+                        <?php if(isset($mail_verif)) echo '<p> Votre adresse email n\'est pas valide </p>';?>
+                        <input type="submit" name="envoi_mail" value="Envoyer"/>      
+                    </form>
             </section>
 
        		<?php include_once('Controller/c_footer.php'); ?>
