25b16b2ad72f9a54c58fba05796e2a570810e7ad
dualshote <benron@hotmail.fr>
2014-03-02 21:51:01 +0100

    modif css des input, select de partout

--- View/v_inscription.php
+++ View/v_inscription.php
@@ -21,28 +21,28 @@
                 <form action="index.php?page=inscription" method="post">
     
                     <?php if(isset($error['pseudo'])) echo '<p>'.$error['pseudo'].'</p>';?>
-                    <label for="pseudo">Pseudo :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/contacts.png"><label for="pseudo">Pseudo :</label>
                     <input type="text" id="pseudo" name="pseudo" value="<?php if(isset($value['pseudo'])) echo $value['pseudo']; ?>" maxlength="30" required/><br/><br/>
     
                     <?php if(isset($error['password'])) echo '<p>'.$error['password'].'</p>';?>
-                    <label for="password">Mot de passe :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/padlock.png"><label for="password">Mot de passe :</label>
                     <input type="password" id="password" name="password" maxlength="30" autocomplete="off" required/><br/><br/>
     
-                    <label for="confirm_password">Confirmation :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/padlock.png"><label for="confirm_password">Confirmation :</label>
                     <input type="password" id="confirm_password" name="confirm_password" maxlength="30" autocomplete="off" required/><br/><br/>
     
                     <?php if(isset($error['familly_name'])) echo '<p>'.$error['familly_name'].'</p>';?>
-                    <label for="familly_name">Nom :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/contacts.png"><label for="familly_name">Nom :</label>
                     <input type="text" id="familly_name" name="familly_name" value="<?php if(isset($value['familly_name'])) echo $value['familly_name']; ?>" maxlength="30" autocomplete="off" required/><br/><br/>
     
                     <?php if(isset($error['name'])) echo '<p>'.$error['name'].'</p>';?>
-                    <label for="name">Prénom :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/contacts.png"><label for="name">Prénom :</label>
                     <input type="text" id="name" name="name" value="<?php if(isset($value['name'])) echo $value['name']; ?>" maxlength="30" autocomplete="off" required/><br/><br/>
             
                     <?php if(isset($error['day_birth'])) echo '<p>'.$error['day_birth'].'</p>';?>
                     <p id="titre_birth">Date de naissance :</p><br/>
                     <?php
-                        echo '<label for="day_birth">Jour</label>
+                        echo '<img id="img_formulaire_inscription" src="Resources/Images/birthday.png"> <label for="day_birth" id="day">Jour</label>
                             <select id="day_birth" name="day_birth">';
                             $i = 1;
                             for($i; $i <= 31; $i++){
@@ -78,19 +78,19 @@
                     ?><br/><br/>
     
                     <?php if(isset($error['street'])) echo '<p>'.$error['street'].'</p>';?>
-                    <label for="street">Adresse :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/home.png"><label for="street">Adresse :</label>
                     <input type="text" id="street" name="street" value="<?php if(isset($value['street'])) echo $value['street']; ?>" maxlength="70" autocomplete="off"/><br/><br/>
     
                     <?php if(isset($error['postal_code'])) echo '<p>'.$error['postal_code'].'</p>';?>
-                    <label for="postal_code">Code postal :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/home.png"><label for="postal_code">Code postal :</label>
                     <input type="text" id="postal_code" name="postal_code" value="<?php if(isset($value['postal_code'])) echo $value['postal_code']; ?>" pattern="\d+" maxlength="5" autocomplete="off"/><br/><br/>
     
                     <?php if(isset($error['city'])) echo '<p>'.$error['city'].'</p>';?>
-                    <label for="city">Ville :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/home.png"><label for="city">Ville :</label>
                     <input type="text" id="city" name="city" value="<?php if(isset($value['city'])) echo $value['city']; ?>" maxlength="30" autocomplete="off"/><br/><br/>
     
                     <?php if(isset($error['mail'])) echo '<p>'.$error['mail'].'</p>';?>
-                    <label for="mail">Mail :</label>
+                    <img id="img_formulaire_inscription" src="Resources/Images/mail.png"><label for="mail">Mail :</label>
                     <input type="email" id="mail" name="mail" value="<?php if(isset($value['mail'])) echo $value['mail']; ?>" maxlength="70" required/><br/><br/>
     
                     <input type="submit" value"S'inscrire" name="submit"/>
