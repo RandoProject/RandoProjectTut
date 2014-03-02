8dd3ac7436d0a0ed37d43c193c01e7dfb6985393
dualshote <benron@hotmail.fr>
2014-03-02 15:29:13 +0100

    modif css contact

--- View/v_contact.php
+++ View/v_contact.php
@@ -22,35 +22,6 @@
             </section>
 
        		<?php include_once('Controller/c_footer.php'); ?>
-
-
-            <style>
-                input[type="text"]:hover,
-                input[type="text"]:focus,
-                input[type="password"]:hover,
-                input[type="password"]:focus,
-                input[type="file"]:hover,
-                input[type="file"]:focus,
-                input[type="range"]:hover,
-                input[type="range"]:focus,
-                input[type="email"]:hover,
-                input[type="email"]:focus,
-                textarea:hover,
-                textarea:focus,
-                select:hover,
-                select:focus{
-                    border-color: rgba(82, 168, 236, 0.8);
-                    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset, 
-                                0 0 8px rgba(82, 168, 236, 0.6);
-                }
-                section#contact form#form_contact{
-                    text-align: center;
-                }
-                section#contact form input#mail, input#nom, input#objet{
-                    margin-left: -37.5%;
-                    width: 350px;
-                }
-            </style>
         </div>
 	</body>
 </html>
