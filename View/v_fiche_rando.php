b1d54eb18e43fdd321c27cbc3a1810331233a991
dualshote <benron@hotmail.fr>
2014-03-03 14:40:05 +0100

    modif css fiche_rando

--- View/v_fiche_rando.php
+++ View/v_fiche_rando.php
@@ -18,7 +18,7 @@
                     <li><img id="img_fiche" src="Resources/Images/longueur.png"/><label>Longueur : </label><?php echo '<div id="div_val_fiche">'.$lenght.'</div>'; ?></li>
                     <li><img id="img_fiche_duree" src="Resources/Images/duree.png"/><label>Durée : </label><?php echo '<div id="div_val_fiche">'.$duration.'</div>'; ?></li>
                     <li><img id="img_fiche_point_eau" src="Resources/Images/eau.png"/><label>Point d'eau : </label><?php echo '<div id="div_val_fiche_eau_denivele">'.$water.'</div>'; ?></li>
-                    <li><img id="img_fiche" src="Resources/Images/longueur.png"/><label>Note : </label><?php echo '<div id="div_val_fiche">'.$note.'</div>'; ?></li>
+                    <li><img id="img_fiche" src="Resources/Images/star-pleine_fiche.png"/><label>Note : </label><?php echo '<div id="div_val_fiche">'.$note.'</div>'; ?></li>
                     <li><?php for($j = 1; $j <= $difficulty; $j++){$difficulte_rond .= '<div id="cercle"></div>'; }?><img id="img_fiche_difficulte" src="Resources/Images/difficulte.png"/><label>Difficulté : </label><?php echo '<div id="div_val_fiche">'.$difficulte_rond.'</div>'; ?></li>
                     <li><img id="img_fiche_denivele" src="Resources/Images/mountain.png"/><label>Dénivelé : </label><?php echo '<div id="div_val_fiche_eau_denivele">'.$altitude.'</div>'; ?></li>
                 </ul><br/>             
