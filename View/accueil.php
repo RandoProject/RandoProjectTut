ffee5682fb9945abae22a8058d6c9f306dad6950
pat42410 <pat42410@hotmail.fr>
2014-03-05 15:29:12 +0100

    Recherche vias la map

--- View/accueil.php
+++ View/accueil.php
@@ -9,28 +9,28 @@
 			$('.map').append('<div class="overlay">').append('<div class="tooltip"></div>')
 			$('.map .tooltip').hide();
 			var regions = [
-				{name : 'Nord Pas de Calais', slug: 'nord-pas-de-calais'},
-				{name : 'Picardie', slug: 'picardie'},
-				{name : 'Haute Normandie', slug: 'haute-normandie'},
-				{name : 'Basse Normandie', slug: 'basse-normandie'},
-				{name : 'Bretagne', slug: 'bretagne'},
-				{name : 'Ile de France', slug: 'ile-de-france'},
-				{name : 'Champagne Ardenne', slug: 'champagne-ardenne'},
-				{name : 'Lorraine', slug: 'lorraine'},
-				{name : 'Alsace', slug: 'alsace'},
-				{name : 'Pays de la Loire', slug: 'pays-de-la-loire'},
-				{name : 'Centre', slug: 'centre'},
-				{name : 'Bourgogne', slug: 'bourgogne'},
-				{name : 'Franche Comte', slug: 'franche-comte'},
-				{name : 'Poitou Charente', slug: 'poitou-charente'},
-				{name : 'Limousin', slug: 'limousin'},
-				{name : 'Auvergne', slug: 'auvergne'},
-				{name : 'Rhône Alpes', slug: 'rhone-alpes'},
-				{name : 'Aquitaine', slug: 'aquitaine'},
-				{name : 'Midi-Pyrénées', slug: 'midi-pyrenees'},
-				{name : 'Languedoc Roussillon', slug: 'languedoc-roussillon'},
-				{name : 'Provence Alpes Côte d\'Azur', slug: 'provence-alpes-cote-d-azur'},
-				{name : 'Corse', slug: 'corse'}
+				{name : 'Nord Pas de Calais', slug: '17'},
+				{name : 'Picardie', slug: '19'},
+				{name : 'Haute Normandie', slug: '11'},
+				{name : 'Basse Normandie', slug: '4'},
+				{name : 'Bretagne', slug: '6'},
+				{name : 'Ile de France', slug: '12'},
+				{name : 'Champagne Ardenne', slug: '8'},
+				{name : 'Lorraine', slug: '15'},
+				{name : 'Alsace', slug: '1'},
+				{name : 'Pays de la Loire', slug: '18'},
+				{name : 'Centre', slug: '7'},
+				{name : 'Bourgogne', slug: '5'},
+				{name : 'Franche Comte', slug: '10'},
+				{name : 'Poitou Charente', slug: '20'},
+				{name : 'Limousin', slug: '14'},
+				{name : 'Auvergne', slug: '3'},
+				{name : 'Rhône Alpes', slug: '22'},
+				{name : 'Aquitaine', slug: '2'},
+				{name : 'Midi-Pyrénées', slug: '16'},
+				{name : 'Languedoc Roussillon', slug: '13'},
+				{name : 'Provence Alpes Côte d\'Azur', slug: '21'},
+				{name : 'Corse', slug: '9'}
 			]
 			
 			// Info-Bulle
@@ -44,7 +44,7 @@
 			// Liens
 			$('.map area').each(function(){
 				var index = $(this).index();
-				$(this).attr('href', 'http://google.com/?q='+regions[index].slug);
+				$(this).attr('href', 'index.php?page=recherche&region='+regions[index].slug);
 			});
 			
 			// Passage sur une région
