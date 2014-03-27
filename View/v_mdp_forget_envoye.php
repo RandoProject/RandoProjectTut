bca69f1797a80e4ba648119ef567a78b994f0eb1
dualshote <benron@hotmail.fr>
2014-03-27 09:33:13 +0100

    modifs commentaire

--- View/v_mdp_forget_envoye.php
+++ View/v_mdp_forget_envoye.php
@@ -3,14 +3,14 @@
 	
 	<?php head("Contact",
 		array(
-			array('type' => 'meta', 'http-equiv' => 'refresh', 'content' => '5; URL=index.php')
+			array('type' => 'meta', 'http-equiv' => 'refresh', 'content' => '5	; URL=index.php')
 		));
 	?>
 	<body>
     	<div id="corps">
 			<?php menu(); ?>
         	
-        		<h2>Votre nouveau mot de passe a été modifié et vous a été envoyé par mail, vous pouvez dès maintenant vous connecter avec !</h2>
+        		<h2>Votre nouveau mot de passe a Ã©tÃ© modifiÃ© et vous a Ã©tÃ© envoyÃ© par mail, vous pouvez dÃ¨s maintenant vous connecter avec !</h2>
 
         	<?php include_once('Controller/c_footer.php'); ?>
         </div>
